4c0e20c32b0203afa0965667ee65cb53b49e64e5
Hugo GYBELS <hugo4242@hotmail.fr>
2017-11-19 13:49:49 +0100

    esaie image

--- app/main/Home.js
+++ app/main/Home.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import {
+  AppRegistry,
   Platform,
+  Image,
   StyleSheet,
   Text,
   View,
   Button,
 } from 'react-native';
 
-
+const remote = 'https://cdn-maf0.heartyhosting.com/sites/muscleandfitness.com/files/media/campus-muscle-trainint-partner-4.jpg';
 
 export default class Home extends Component<{}> {
-	
+
   render() {
+    const resizeMode = 'center';
+
     return (
        <View style={styles.container}>
+          <Image
+        style={{
+          flex: 1,
+          resizeMode,
+          flex: .5,
+        backgroundColor: '#FF3366',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+        }}
+        source={{ uri: remote }}
+      />
+
               <View style={styles.halfHeight} >
                 <Text style={styles.caption}>
                   Create new plan
@@ -31,15 +48,18 @@ export default class Home extends Component<{}> {
                   My followed
                 </Text>
               </View>
+
           </View>
     );
   }
 }
 
+AppRegistry.registerComponent('BackgroundImage', () => BackgroundImage);
+
 var styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column'
+        flexDirection: 'column',
     },
     halfHeight: {
         flex: .5,
