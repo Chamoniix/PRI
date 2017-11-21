1abe0a642bf1a2824eb45290d6f3b31b316a727b
Hugo GYBELS <hugo4242@hotmail.fr>
2017-11-21 11:21:35 +0100

    photo mieux

--- app/main/Home.js
+++ app/main/Home.js
@@ -7,29 +7,23 @@ import {
   Text,
   View,
   Button,
+  Dimensions
 } from 'react-native';
 
-const remote = 'https://cdn-maf0.heartyhosting.com/sites/muscleandfitness.com/files/media/campus-muscle-trainint-partner-4.jpg';
+//Dimension of screen :
+var w = Dimensions.get('window').width;
+var h = Dimensions.get('window').height;
+
 
 export default class Home extends Component<{}> {
 
   render() {
-    const resizeMode = 'center';
-
+    let imgUrl = require('../img/DSC_6536.jpg');
     return (
-       <View style={styles.container}>
-          <Image
-        style={{
-          flex: 1,
-          resizeMode,
-          flex: .5,
-        backgroundColor: '#FF3366',
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'center'
-        }}
-        source={{ uri: remote }}
-      />
+          <View style={styles.container}>
+              <View>
+                <Image source={imgUrl} style={{height: w*0.328, width: w}}/>
+              </View>
 
               <View style={styles.halfHeight} >
                 <Text style={styles.caption}>
