67e89056b350967895f9722d57e63d31587e1c0f
BenjiPich <benjamin.pichon@telecom-st-etienne.fr>
2018-01-07 19:49:10 +0100

    duree, diff

--- app/main/CreatePlan2.js
+++ app/main/CreatePlan2.js
@@ -1,24 +1,65 @@
 import React, { Component } from 'react';
 import {
+  AppRegistry,
   Platform,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Dimensions,
+  TouchableHighlight,
+  ActivityIndicator,
+  ListView,
+  Alert,
 } from 'react-native';
-import Navigation from './Navigation';
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
 
 import {objectif} from './CreatePlan';
 
-export default class CreatePlanBis extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-	  
-        <Text style={styles.welcome}>
-          Obj chosen: {objectif}
-        </Text>  
-      </View>
-    );
+var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+export default class CreatePlanBis extends Component {
+        
+    ListViewItemSeparator = () => {
+        return (
+            <View style={{height: .5, width: "100%", backgroundColor: "#000",}}/>
+        );
+    }
+    
+    render() {        
+        return (
+          <View style={styles.container}>
+            <View>
+                <View style={{backgroundColor:'red'}}>
+                    <Text style={styles.textTitle}>
+                    Créer mon plan d'entrainement
+                    </Text>
+                </View>
+                <Text>Texte de présentation</Text>
+            </View>
+            <View>
+                <Text style={styles.welcome}>
+                  Choisissez votre durée:
+                </Text> 
+                <ListView
+                    dataSource={ds.cloneWithRows(["1 mois", "3 mois", "6 mois"])}
+                    renderSeparator={this.ListViewItemSeparator}
+                    renderRow={(rowData) => <Text style={styles.rowViewContainer}>{rowData}</Text>}
+                />
+                <Text style={styles.welcome}>
+                  Choisissez votre difficulté:
+                </Text> 
+                <ListView
+                    dataSource={ds.cloneWithRows(["Débutant", "Intermédiaire", "Confirmé"])}
+                    renderSeparator={this.ListViewItemSeparator}
+                    renderRow={(rowData) => <Text style={styles.rowViewContainer}>{rowData}</Text>}
+                />
+            </View>
+          </View>
+        );
   }
 }
 
@@ -29,9 +70,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+    textTitle:{
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+    },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
+   rowViewContainer: {
+        fontSize: 20,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+      }
 });
