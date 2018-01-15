a8daee9c6ae7f15ced510ed7f68aab9f160ee209
BenjiPich <benjamin.pichon@telecom-st-etienne.fr>
2018-01-15 15:29:15 +0100

    Changement de style des elements seletiones

--- app/main/CreatePlan.js
+++ app/main/CreatePlan.js
@@ -25,19 +25,33 @@ export default class CreatePlan extends Component {
         super(props);
         this.state = {
             isLoading: true,
-            dataSourceObj: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+            dataSourceObj: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 === r2}),
+            dataSourceAct: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 === r2}),
+            selectedAct: '',
+            selectedObj: '',
         }
     }
     
-    GoToNextStep(obj){
-        objectif = obj;
-        this.props.navigation.navigate('CreatePlanBis');
+    componentDidMount(){
+        this.getActivites();
     }
     
-    GetObj(activity){
-        this.setState({
-            isLoading: true,
+    getActivites = () => {
+        return fetch('http://213.32.66.63/appliPP/getActivites.php')
+        .then((response) => response.json())
+        .then((res) => {
+            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 === r2});
+            this.setState({
+                isLoading: false,
+                dataSourceAct: ds.cloneWithRows(res),
+            })
+        })
+        .catch((error) => {
+            console.error(error);
         });
+    }
+    
+    getObjectifs(activity){
         return fetch('http://213.32.66.63/appliPP/getObjectifs.php',
         {
             method: "POST", 
@@ -51,10 +65,10 @@ export default class CreatePlan extends Component {
         })
         .then((response) => response.json())
         .then((res) => {
-            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+            let dss = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 === r2});
             this.setState({
                 isLoading: false,
-                dataSourceObj: ds.cloneWithRows(res),
+                dataSourceObj: dss.cloneWithRows(res),
             })
         })
         .catch((error) => {
@@ -62,32 +76,70 @@ export default class CreatePlan extends Component {
         });
     }
     
-    componentDidMount(){
-        return fetch('http://213.32.66.63/appliPP/getActivites.php')
-        .then((response) => response.json())
-        .then((res) => {
-            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-            this.setState({
-                isLoading: false,
-                dataSourceAct: ds.cloneWithRows(res),
-            })
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    goToNextStep(){
+        objectif = this.state.selectedObj;
+        //Alert.alert(objectif);
+        this.props.navigation.navigate('CreatePlanBis');
     }
     
-    ListViewItemSeparator = () => {
-        return (
+  renderAct(rowData, rowID) {
+    return (
+      <TouchableHighlight onPress={() => {
+        this.selectAct(rowData, rowID)
+      }}>
+        <View style={this.state.selectedAct == rowData.activite_id
+          ? styles.weekRowSelected
+          : styles.weekRow}>
+          <Text style={this.state.selectedAct == rowData.activite_id
+            ? styles.weekTextSelected
+            : styles.weekText}>
+            {rowData.activite_nom}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+  
+  selectAct(rowData, rowID) {
+    this.setState({selectedAct: rowData.activite_id, isLoading: true});
+    this.getActivites();
+    this.getObjectifs(rowData.activite_id);
+  }
+  
+  renderObj(rowData, rowID) {
+    return (
+      <TouchableHighlight onPress={() => {
+        this.selectObj(rowData, rowID)
+      }}>
+        <View style={this.state.selectedObj == rowData.objectif_id
+          ? styles.weekRowSelected
+          : styles.weekRow}>
+          <Text style={this.state.selectedObj == rowData.objectif_id
+            ? styles.weekTextSelected
+            : styles.weekText}>
+            {rowData.objectif_nom}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+  
+  selectObj(rowData, rowID) {
+    this.setState({selectedObj: rowData.objectif_id, isLoading: true});
+    this.getObjectifs(this.state.selectedAct);
+  }
+    
+  ListViewItemSeparator = () => {
+      return (
             <View style={{height: .5, width: "100%", backgroundColor: "#000",}}/>
-        );
-    }
+       );
+  }
     
     render() {
         if(this.state.isLoading){
             return(
-                <View style={{flex: 1, paddingTop: 20}}>
-                    <ActivityIndicator />
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <ActivityIndicator size='large' color='rgb(217,217,217)'/>
                 </View>
             );
         }
@@ -109,20 +161,25 @@ export default class CreatePlan extends Component {
                 <ListView
                     dataSource={this.state.dataSourceAct}
                     renderSeparator={this.ListViewItemSeparator}    
-                    renderRow={(rowData) => <Text style={styles.rowViewContainer} onPress={this.GetObj.bind(this, rowData.activite_id)}>
-                    {rowData.activite_nom}</Text>}
+                    renderRow={this.renderAct.bind(this)}
                 />
+                <Text style={styles.welcome}>
+                  Choisissez votre objectif:
+                </Text> 
                 <ListView
                     dataSource={this.state.dataSourceObj}
                     renderSeparator={this.ListViewItemSeparator}
-                    renderRow={(rowData) => <Text style={styles.rowViewContainer} onPress={this.GoToNextStep.bind(this, rowData.objectif_id)}>
-                    {rowData.objectif_nom}</Text>}
+                    renderRow={this.renderObj.bind(this)}
                 />
+                <TouchableHighlight onPress={() => this.goToNextStep()}>
+                    <Text>Prochaine Etape</Text>
+                </TouchableHighlight>
             </View>
           </View>
         );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -152,6 +209,19 @@ const styles = StyleSheet.create({
         paddingTop: 10,
         paddingBottom: 10,
         backgroundColor: 'blue',
-   }
+   },
+   weekRowSelected : {
+  },
+  weekRow: {
+  },
+  weekTextSelected: {
+    fontSize: 22,
+    color: 'yellow',
+  },
+  weekText: {
+    fontSize: 20,
+    color: 'blue',
+  }
 });
+
 export {objectif};
